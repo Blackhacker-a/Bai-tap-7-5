e2223316b8e679b415e49620bbae4eb6b1ce8495
Blackhacker-a <64885014+Blackhacker-a@users.noreply.github.com>
2020-05-08 23:21:55 +0700

    em sửa lại cho đúng đề ạ(yêu cầu nhập số nguyên)

--- Bai tap 3.cpp
+++ Bai tap 3.cpp
@@ -1,19 +1,19 @@
 #include<stdio.h>
-#include<math.h> //trong bai nay em phai dung thu vien math.h, vi e cung khong ro dung moi stdio.h thi choi the nào.
+#include<math.h> 
 int main(){
-	float a,b,c,d;// khai bao bien co the mot dong nhung em de hai dong vi no co vai tro khac nhau
+	int a,b,c,d;
 	float x,x1,x2;
 	printf("Nhap vao he so a cua pt bac 2 ");
-	scanf("%f",&a);
+	scanf("%d",&a);
 	printf("Nhap vao he so b cua pt bac 2 ");
-	scanf("%f",&b);
+	scanf("%d",&b);
 	printf("Nhap vao he so c cua pt bac 2 ");
-	scanf("%f",&c);
+	scanf("%d",&c);
 	d=b*b-4*a*c;
 	//em cách doan code nay ra cho de fix bug
 	if(a==0){
 	if(b!=0){
-	x=-c/b;
+	x=(float)-c/b;
 	printf("Phuong trinh co nghiem la %f\n",x);
 	}else{
 	if(c==0){
@@ -29,7 +29,7 @@ int main(){
     printf("Phuong trinh vo nghiem\n");
 	}
 	if(d==0){
-	x1=x2=(-b)/(2*a);
+	x1=x2=(float)(-b)/(2*a);
 	printf("Phuong trinh co nghiem la x1=x2=%f\n",x1);
 	}
 	if(d>0){
